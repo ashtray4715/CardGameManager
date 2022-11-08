a03c679ab9bc7dc41acf0b41ae2592e57839671c
Ashtray4715 <gobinda.paul.4715@gmail.com>
2022-11-09 03:48:21 +0600

    Creating a simple package structure and making the splash screen as launch activity

--- app/src/main/java/com/ashtray/card/game/manager/features/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/ashtray/card/game/manager/features/splash/SplashScreenActivity.kt
@@ -1,9 +1,12 @@
-package com.ashtray.card.game.manager
+package com.ashtray.card.game.manager.features.splash
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ashtray.card.game.manager.R
 
-class MainActivity : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
