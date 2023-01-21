fdf45f910db6ef792df2b38853b852740629ce15
Ashtray4715 <gobinda.paul.4715@gmail.com>
2023-01-22 01:15:00 +0600

    Making the repository singleton

--- app/src/main/java/com/ashtray/card/game/manager/apps/MyRepository.kt
+++ app/src/main/java/com/ashtray/card/game/manager/apps/MyRepository.kt
@@ -2,6 +2,9 @@ package com.ashtray.card.game.manager.apps
 
 import com.ashtray.card.game.manager.database.main.AppDao
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class MyRepository @Inject constructor(private val appDao: AppDao) {
+
 }
